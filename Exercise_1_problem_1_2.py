eac112ad17e44f1a672ead396f348a827d42035c
yutokajiki <>
2021-05-31 01:36:11 +0000

    I create create_point_geom()

--- Exercise_1_problem_1_2.py
+++ Exercise_1_problem_1_2.py
@@ -11,7 +11,8 @@
 
 from shapely.geometry import Point, LineString, Polygon
 #YOUR CODE HERE 1 to define create_point_geom()
-
+def create_point_geom(x,y):
+  return(Point(x,y))
 # Test your function by running these code cells:
 
 # CODE FOR TESTING YOUR SOLUTION
@@ -67,7 +68,7 @@ except Exception as e:
 # Demonstrate the usage of the function. For example, create a Polygon with three points: `(45.2, 22.34)`, `(100.22, -3.20)` & `(70.0, 10.20)`.
 
 # YOUR CODE HERE 5 to define poly1 with three points
-poly1 = 
+#poly1 = 
 
 # CODE FOR TESTING YOUR SOLUTION
 print(poly1)
